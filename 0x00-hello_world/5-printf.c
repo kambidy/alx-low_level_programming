ce602e4fc95436d94d999327ee7f8e4f5c8f03c8
kambidy <ryanmulwa100@gmail.com>
2023-03-16 13:49:40 -0700

    learning c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a value
+ * Description: prints values of a string
+ * Return: it returns nothing
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,")
+	return (0);
+}
