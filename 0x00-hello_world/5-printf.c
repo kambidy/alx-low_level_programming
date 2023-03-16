967d07b870cae3d615a3a03c7a61e190ae07ded8
kambidy <ryanmulwa100@gmail.com>
2023-03-16 13:52:46 -0700

    learning c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,")
-	return (0);
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
+~         
