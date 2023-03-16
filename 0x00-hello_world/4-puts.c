44f859b218385aef27cf65b70d56bfb42bd3ba01
kambidy <ryanmulwa100@gmail.com>
2023-03-16 12:25:41 -0700

    learning c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int main(void)
+int main(void)/* running the prog */
 {
-puts("\"Pogramming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
