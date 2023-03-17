f9c82899c2ee05601c1c2de39d489b022f350bac
kambidy <ryanmulwa100@gmail.com>
2023-03-17 12:18:44 -0700

    learning c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints a value
+ * Description: prints values of a string
+ * Return: it returns nothing
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
