6714dbe905e807e0abbd42c4f533dba68c590552
kambidy <ryanmulwa100@gmail.com>
2023-03-23 06:00:59 -0700

    learning c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,8 @@
  */
 void positive_or_negative(int i)
 {
-if (i < 0)
-printf("%d is negative", i);
-else
-printf("%d is positive", i);
-return (0);
+	if (i < 0)
+	printf("%d is negative", i);
+	else
+	printf("%d is positive", i);
 }
