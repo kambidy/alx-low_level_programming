0c5c9c484f4fe28b17490f1947effdb6a8a9c7b7
kambidy <ryanmulwa100@gmail.com>
2023-03-27 00:57:44 -0700

    learning c git push

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * reset_to_98 - takes pointer n
+ * @n: is the input
+ * Return: returns n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (*n);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int_putchar(char c);
+void reset_to_98(int *n);
+#endif
