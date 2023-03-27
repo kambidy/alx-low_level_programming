bcc0d9b366e5aa93966c25fcf39866f4b604d415
kambidy <ryanmulwa100@gmail.com>
2023-03-27 01:55:14 -0700

    learning c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swaps values betweeen variables
+ * Description: swaps values
+ * @a: value
+ * @b: value
+ * Return: reurns a and b
+ */
+void swap_int(int *a, int *b)
+{
+	int *a = &b;
+	int *b = &a;
+
+}
