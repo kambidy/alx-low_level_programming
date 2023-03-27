0c00e8cbdc549bc62d23be8d514a164f38b15baf
kambidy <ryanmulwa100@gmail.com>
2023-03-27 08:19:58 -0700

    learning c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
