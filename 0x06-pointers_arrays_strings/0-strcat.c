7873f418b1e7b28cb4779ef034c5710013922076
kambidy <ryanmulwa100@gmail.com>
2023-03-29 07:31:43 -0700

    learning c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - concatinates 2 strings
+ * Description: above is the descript
+ * @dest: does what it does
+ * @src: does that
+ * Return: returns s1 + s2
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(s1, s1);
+}
