19d0fd1cf884371aee7717032cabe9214905e00f
kambidy <ryanmulwa100@gmail.com>
2023-03-30 04:15:11 -0700

    learnig c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(s1, s1);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
