5b59567e529d48f7d5b93e7d85a8eef016e8716f
kambidy <ryanmulwa100@gmail.com>
2023-04-27 13:10:15 -0700

    learningc

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,8 +6,4 @@ main:
 	  call  printf
 	  mov   eax, 0
 	  ret
-<<<<<<< HEAD
-format: db `Hello, Holberton\n ,0
-=======
 format: db `Hello, Holberton\n`,0
->>>>>>> 977db24706100f7d11bfd4efb11c8c47a328000f
